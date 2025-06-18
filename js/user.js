0c06ac700465fee3b877b459d06c3d923c5a297e
rommel dalisay <rommelmdalisay@gmail.com>
2025-06-18 11:15:31 +0800

    profile

---
+++ js/user.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    const url = 'http://172.34.98.64:4000/'
+    $('#avatar').on('change', function () {
+        const file = this.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = function (e) {
+                console.log(e.target.result)
+                $('#avatarPreview').attr('src', e.target.result);
+            };
+            reader.readAsDataURL(file);
+        }
+    });
+})
