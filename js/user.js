74c33cfcf5928c93aa3bca91605d52e57012a374
rommel dalisay <rommelmdalisay@gmail.com>
2025-06-21 08:22:31 +0800

    login

--- js/user.js
+++ js/user.js
@@ -11,4 +11,48 @@ $(document).ready(function () {
             reader.readAsDataURL(file);
         }
     });
+
+    $("#login").on('click', function (e) {
+        e.preventDefault();
+
+        let email = $("#email").val()
+        let password = $("#password").val()
+        let user = {
+            email,
+            password
+        }
+        $.ajax({
+            method: "POST",
+            url: `${url}api/v1/login`,
+            data: JSON.stringify(user),
+            processData: false,
+            contentType: 'application/json; charset=utf-8',
+            dataType: "json",
+            success: function (data) {
+                console.log(data);
+                Swal.fire({
+                    text: data.success,
+                    showConfirmButton: false,
+                    position: 'bottom-right',
+                    timer: 1000,
+                    timerProgressBar: true
+
+                });
+                sessionStorage.setItem('userId', JSON.stringify(data.user.id))
+                window.location.href = 'profile.html'
+            },
+            error: function (error) {
+                console.log(error);
+                Swal.fire({
+                    icon: "error",
+                    text: error.responseJSON.message,
+                    showConfirmButton: false,
+                    position: 'bottom-right',
+                    timer: 1000,
+                    timerProgressBar: true
+
+                });
+            }
+        });
+    });
 })
