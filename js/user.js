6d3f6964b18fa10c9cdca75f8e9193180dd7e0ca
rommel dalisay <rommelmdalisay@gmail.com>
2025-06-21 08:37:09 +0800

    login register

--- js/user.js
+++ js/user.js
@@ -1,5 +1,38 @@
 $(document).ready(function () {
     const url = 'http://172.34.98.64:4000/'
+
+    $("#register").on('click', function (e) {
+        e.preventDefault();
+        let name = $("#name").val()
+        let email = $("#email").val()
+        let password = $("#password").val()
+        let user = {
+            name,
+            email,
+            password
+        }
+        $.ajax({
+            method: "POST",
+            url: `${url}api/v1/register`,
+            data: JSON.stringify(user),
+            processData: false,
+            contentType: 'application/json; charset=utf-8',
+            dataType: "json",
+            success: function (data) {
+                console.log(data);
+                Swal.fire({
+                    icon: "success",
+                    text: "register success",
+                    position: 'bottom-right'
+
+                });
+            },
+            error: function (error) {
+                console.log(error);
+            }
+        });
+    });
+
     $('#avatar').on('change', function () {
         const file = this.files[0];
         if (file) {
